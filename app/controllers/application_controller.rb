35463376310b3f2b746ade57006a0e8302c0cf7c
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-16 14:35:06 +0200

    update pundit messages and improve ux

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
-  rescue_from Pundit::NotAuthorizedError, with: :pundit_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
 
@@ -23,8 +23,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def pundit_not_authorized
-    flash[:alert] = "You are not authorized to perform this action"
-    redirect_back(fallback_location: root_path)
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+    redirect_to(request.referrer || root_path)
   end
 end
--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -12,4 +12,11 @@ class QuestionPolicy < ApplicationPolicy
   def destroy?
     user == record.user
   end
+
+  def view?
+    # anyone can view a question whose group is nil
+    # group questions can only be viewed by group participants
+    # the question asker always has access to his/her question even if he/she leaves the group
+    record.group.nil? || GroupPolicy.new(user, record.group).participate? || record.user == user
+  end
 end
