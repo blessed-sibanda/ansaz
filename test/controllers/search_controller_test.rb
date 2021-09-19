111b3ba5fa0efd70a87df1228db66c0ccc569f57
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-19 17:04:09 +0200

    attempt search controller test

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in create(:user)
     get search_index_url
     assert_response :success
   end
