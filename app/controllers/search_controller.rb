f495f10fe7a20111207360123bb10894b692a900
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-20 07:46:17 +0200

    remove search controller

--- app/controllers/search_controller.rb
+++
@@ -1,7 +0,0 @@
-class SearchController < ApplicationController
-  def index
-    keyword = params[:keyword]
-    @questions = keyword.nil? ? nil : Question::Searcher.call(keyword: keyword)
-    render "questions/index"
-  end
-end
--- test/controllers/search_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class SearchControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    sign_in create(:user)
-    get search_index_url
-    assert_response :success
-  end
-end
